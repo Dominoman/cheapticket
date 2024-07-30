614ebfb77e0ac49d287657c404962fedbb37cff0
Dominoman <olaca@freemail.hu>
2024-07-30 09:19:40 +0200

    Retry failed download

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from database import Database
 from kiwi import Tequila
 
 
-def savefile(json_data:dict):
+def savefile(json_data: dict):
     json_text = json.dumps(json_data, indent=4)
     fname = f"{config.SAVEDIR}/{datetime.now().strftime('%Y%m%d%H%M%S')}-{start_date.strftime('%Y%m')}.json"
 
@@ -22,23 +22,27 @@ def savefile(json_data:dict):
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    logging.basicConfig(filename="app.log",level=logging.DEBUG,format="{asctime} - {levelname} - {message}", style="{", datefmt="%Y-%m-%d %H:%M")
+    logging.basicConfig(filename="app.log", level=logging.DEBUG, format="{asctime} - {levelname} - {message}",
+                        style="{", datefmt="%Y-%m-%d %H:%M")
     logging.info("Start")
     kiwi = Tequila(config.APIKEY)
-    db=Database(config.DB_FILENAME, config.DB_DEBUG)
+    db = Database(config.DB_FILENAME, config.DB_DEBUG)
     start_date = datetime.now()
     for _ in range(12):
         next_month = start_date.month % 12 + 1
         next_year = start_date.year + (start_date.month // 12)
         start_date = datetime(next_year, next_month, 1)
         end_date = datetime(next_year, next_month, calendar.monthrange(next_year, next_month)[1])
-        logging.info("Search")
-        result = kiwi.search("BUD,VIE", start_date, end_date, "BKK", 5, 18, max_fly_duration=17, max_stopovers=1,
-                             limit=1000)
-        logging.info(kiwi.status_code)
-        savefile(result)
-        if kiwi.status_code==200:
-            logging.info("Insert DB")
-            db.insert_json(result,kiwi.search_url)
+        i = 3
+        while i > 0:
+            i -= 1
+            logging.info("Search")
+            result = kiwi.search("BUD,VIE", start_date, end_date, "BKK", 5, 18, max_fly_duration=17, max_stopovers=1,
+                                 limit=1000)
+            savefile(result)
+            if kiwi.status_code == 200:
+                logging.info("Insert DB")
+                db.insert_json(result, kiwi.search_url)
+                i = 0
 
     logging.info("Finished")
