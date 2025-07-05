098e1f1c989d4d83583df4a211ff01c272f99180
Dominoman <olaca@freemail.hu>
2025-07-05 09:57:16 +0200

    BUD & VIE

--- stat_utils.py
+++ stat_utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import datetime
 import os
 
 import jinja2
@@ -26,15 +27,25 @@ def generate_template(itineraries:dict[str,list])->str:
     return template.render(itineraries=itineraries['itineraries'])
 
 def send_stat_mail(db:Database,send_to:str)->None:
-    cheapest_itineraries = (db.session.query(Itinerary)
+    cheapest_itineraries_BUD = (db.session.query(Itinerary)
                             .join(Itinerary.search)  # or .join(Search) if not using relationship
                             .filter(Search.actual == 1)
+                            .filter(Itinerary.flyFrom == "BUD")
                             .order_by(Itinerary.price)
                             .limit(5).all())
-    itineraries = [row.rowid for row in cheapest_itineraries]
+    itineraries = [row.rowid for row in cheapest_itineraries_BUD]
+    cheapest_itineraries_VIE = (db.session.query(Itinerary)
+                            .join(Itinerary.search)  # or .join(Search) if not using relationship
+                            .filter(Search.actual == 1)
+                            .filter(Itinerary.flyFrom == "BUD")
+                            .order_by(Itinerary.price)
+                            .limit(5).all())
+    itineraries += [row.rowid for row in cheapest_itineraries_VIE]
+
     result = db.fill_missing_itineraries(itineraries)
     html = generate_template(result)
-    sendmail(send_to, "Itineraries", html)
+    subject = f"Bangkok repülővel {datetime.date.today().strftime('%Y-%m-%d')}"
+    sendmail(send_to, subject, html)
 
 if __name__ == "__main__":
     db=Database(config.DB_FILENAME,config.DB_DEBUG)
