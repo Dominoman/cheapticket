4e28645e98de275dbf36ee8b10723bc0cf8afaa5
Dominoman <olaca@freemail.hu>
2025-07-28 10:41:32 +0200

    Website fix

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from config.config import config
 from webapp import app
 
 if __name__ == "__main__":
-    app.run(host=config.FLASK_HOST,port=config.FLASK_PORT,debug=config.FLASK_DEBUG)
+    app.run(debug=True)
