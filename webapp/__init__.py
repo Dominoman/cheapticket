bc88d538d57706b8d92f6c0a5f47ba56d7b46a56
Dominoman <olaca@freemail.hu>
2025-08-03 17:09:28 +0200

    Move static url under flight/

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -5,7 +5,11 @@ from common.stat_utils import punctuation, to_time, up_n_down
 from config.config import config
 
 db = Database(config.DB_FILENAME,config.DB_DEBUG)
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path = '/flight/static',
+    static_folder = 'static'
+)
 
 from webapp.flights.views import flight_blueprint
 app.register_blueprint(flight_blueprint)
