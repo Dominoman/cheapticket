8573543365c79e64014b1c7004c7db8910f7f4d1
Dominoman <olaca@freemail.hu>
2025-08-03 18:57:12 +0200

    Graph in website

--- webapp/flights/views.py
+++ webapp/flights/views.py
@@ -12,7 +12,6 @@ def home():
 
 @flight_blueprint.route('/flight/<itinerary_id>')
 def flight(itinerary_id):
-    # get Itinerary by itinerary_id
     prices = (
         db.session.query(Itinerary.rowid,Itinerary.price, Search.timestamp)
         .join(Search, Itinerary.search_id == Search.rowid)
@@ -20,9 +19,11 @@ def flight(itinerary_id):
         .order_by(Search.timestamp)
         .all()
     )
-    if prices is None:
+    if not prices:
         abort(404)
     itinerary=[prices[-1].rowid]
+    labels = [p.timestamp.strftime("%Y.%m.%d") for p in prices]
+    values = [int(p.price) for p in prices]
     result = db.fill_missing_itineraries(itinerary)
     logos = collect_logos(result)
-    return render_template('flight.html',itinerary=result['itineraries'][0],prices=prices,logos=logos)
+    return render_template('flight.html',itinerary=result['itineraries'][0],logos=logos,labels=labels,prices=values)
